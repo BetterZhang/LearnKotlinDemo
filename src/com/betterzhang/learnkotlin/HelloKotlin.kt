817d33020a5760686a3c00c31b6f8bc0f0fc6a69
zhangzhongqiang <zhangzhongqiang@jsdttec.com>
2017-06-20 17:31:13 +0800

    Initial commit

---
+++ src/com/betterzhang/learnkotlin/HelloKotlin.kt
@@ -0,0 +1,13 @@
+package com.betterzhang.learnkotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/20 下午 5:29
+ * Desc   : description
+ */
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+}
