da65833229033b834540e357c8bc20313cbe2bb2
zhangzhongqiang <zhangzhongqiang@jsdttec.com>
2017-06-23 09:48:57 +0800

    Kotlin第三课--类和对象---属性和字段

---
+++ src/com/betterzhang/learnkotlin/kotlin/KotlinLesson4_2.kt
@@ -0,0 +1,9 @@
+package com.betterzhang.learnkotlin.kotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/23 上午 9:48
+ * Desc   : 类和对象---属性和字段
+ */
