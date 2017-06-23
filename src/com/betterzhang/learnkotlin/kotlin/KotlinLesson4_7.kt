c2ea424b4d783f1500da815da00285ccf741e16a
BetterZhang <betterzhang.dev@gmail.com>
2017-06-23 15:22:18 +0800

    Kotlin第三课--类和对象---密封类

---
+++ src/com/betterzhang/learnkotlin/kotlin/KotlinLesson4_7.kt
@@ -0,0 +1,22 @@
+package com.betterzhang.learnkotlin.kotlin
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author : Andrew Zhang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2017/06/23 下午 3:14
+ * Desc   : 类和对象---密封类
+ */
+
+// 虽然密封类也可以有子类，但是所有子类都必须在与密封类自身相同的文件中声明
+sealed class Expr
+
+data class Const(val number: Double) : Expr()
+data class Sum(val e1: Expr, val e2: Expr) : Expr()
+object NotANumber : Expr()
+
+fun eval(expr: Expr): Double = when(expr) {
+    is Const -> expr.number
+    is Sum -> eval(expr.e1) + eval(expr.e2)
+    NotANumber -> Double.NaN
+}
